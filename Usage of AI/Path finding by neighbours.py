5747814c4cb4b34fd0c9f59393d132d6e77333ad
Mateusz Błażejczyk <nibek1000@gmail.com>
2024-03-06 09:15:28 +0100

    Update Path finding by neighbours.py

--- Usage of AI/Path finding by neighbours.py
+++ Usage of AI/Path finding by neighbours.py
@@ -1,7 +1,19 @@
 import math
 
-punkty = {"A": [1, 1], "B": [5,8], "C": [7, 12], "D": [2, 9], "E": [7, 2], "F": [1, 12], "G": [4, 2]}
-startowy = "G"
+punkty = {}
+while True:
+    print(punkty)
+    print("Podaj kolejno nazwe punktu a później koordynaty. Jeśli podałeś już punkty, wpisz stop")
+    tmp1 = str(input("Podaj nazwe punktu: "))
+    if tmp1 == "stop":
+        break
+    tmp2 = int(input("Podaj położenie X: "))
+    tmp3 = int(input("Podaj położenie Y: "))
+    tmp4 = [tmp2, tmp3]
+    punkty[tmp1] = tmp4
+    print("------------Dodano-----------")
+startowy = str(input("Podaj punkt startowy: "))
+print("--------------------------")
 kordynaty_strt = punkty[startowy]
 trasa = [startowy]
 suma_odleglosci = []
@@ -28,6 +40,9 @@ def szukanie_trasy(strt):
         szukanie_trasy(min(odleglosci, key=odleglosci.get))
 
 szukanie_trasy(startowy)
+print("Kolejnośc odwiedzanych punktów:")
 print(trasa)
+print("\nOdległości pomiędzy punktami:")
 print(suma_odleglosci)
+print("\nSuma odległości:")
 print(sum(suma_odleglosci))
